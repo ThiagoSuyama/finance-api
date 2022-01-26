577cac0226bf8be8c674697b8e644b32c5e0259c
Bruno Quintiliano <bruno.quintiliano@hotmail.com>
2022-01-26 19:57:36 -0300

    create crud_user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,14 +3,14 @@ import bcrypt from 'bcrypt'
 
 class UserController {
   async create (req, res) {
+    const {id, name, email, password} = req.body
     
-    const userExists = await User.findOne({where: { email : req.body.email }})
+    const userExists = await User.findOne({where: { email : email }})
 
     if ( userExists ){
       return res.status(400).json({ error: 'User already exists. '});
     }
-
-    const {id, name, email, password} = req.body
+    
     const password_hash  = await bcrypt.hash(password, 10)
 
     const user =  await User.create({ id, name, email, password_hash});
@@ -18,6 +18,52 @@ class UserController {
     return res.json(user)
   }
 
+  async list (req, res) {
+      
+    const users = await User.findAll()
+    
+    return res.json(users)
+  }
+  
+  async update (req, res){ 
+
+    const {name, email, password} = req.body
+
+    const userExists = await User.findOne({where: { email : email }})
+    
+    if(!userExists){
+      return res.status(400).json({ error: 'User not found. '});
+    }
+
+    if(password){
+      const password_hash  = await bcrypt.hash(password, 10)
+      await User.update({ name: name , email: email, password_hash: password_hash}, {where: {email:email}} );
+      
+      const userUpdate = await User.findOne({where: { email : email }})
+
+      return res.json(userUpdate)
+    }
+        
+    await User.update({ name: name }, {where: {email:email}} );
+    
+    const userUpdate = await User.findOne({where: { email : email }})
+
+    return res.json(userUpdate)
+  }
+
+  async delete(req, res){
+    const {id} = req.params
+
+    const userExists = await User.findOne({where: { id : id }})
+   
+    if(!userExists){
+      return res.status(400).json({ error: 'User not found. '});
+    }
+
+    await User.destroy({where:{id: id}})
+    return res.status(200).json({ mgs: 'User deleted.', user: userExists.email});
+
+  }
 }
 
 export default new UserController();
